d2d2d3b7f43e6e7a018fcb4644395ac0f9529890
YukselG PC <Guray95@hotmail.com>
2025-09-08 14:16:03 +0200

    create AccountController in backend and ímplement logout user endpoint

---
+++ ForumBackend/ForumBackend/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using ForumBackend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly SignInManager<User> _signInManager;
+        public AccountController(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("logout")]
+        [Authorize] // only authorized (logged in) users can call this endpoint
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync(); // remove cookie
+
+            return Ok(new { message = "Logged out successfully" });
+        }
+    }
+}
