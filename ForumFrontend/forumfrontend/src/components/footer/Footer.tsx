922fdccff8f7c69f36c39c4712737bd57f6a7289
YukselG PC <Guray95@hotmail.com>
2025-09-26 16:34:34 +0200

    update footer year

--- ForumFrontend/forumfrontend/src/components/footer/Footer.tsx
+++ ForumFrontend/forumfrontend/src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 export default function Footer() {
 	return (
 		<div className="footer-content">
-			<p>&copy; 2024 Yuksel's Forum. All Rights Reserved.</p>
+			<p>&copy; 2025 Yuksel's Forum. All Rights Reserved.</p>
 		</div>
 	);
 }
